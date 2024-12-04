b4a65c5605fdc6d8601d533fdc51665500b7362f
Patrick Odeke <patrickodekeofficial@gmail.com>
2024-12-04 16:01:50 +0300

    window object SSR issue fix #2

--- src/components/location.js
+++ src/components/location.js
@@ -14,24 +14,30 @@ const Location = () => {
       iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
       iconUrl: require('leaflet/dist/images/marker-icon.png').default,
       shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
-    });
+    });    
+
+    if (typeof window !== "undefined") {
+      return (
+        <MapContainer center={position} zoom={13} style={{ height: '360px', width: '100%' }}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={position}>
+            <Popup>
+              Our Main Office
+            </Popup>
+          </Marker>
+        </MapContainer>
+      );
+    }
   }, []);
 
-  if (typeof window !== "undefined") {
-    return (
-      <MapContainer center={position} zoom={13} style={{ height: '360px', width: '100%' }}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker position={position}>
-          <Popup>
-            Our Main Office
-          </Popup>
-        </Marker>
-      </MapContainer>
-    );
-  }
+  return (
+    <div>
+      <h2>map</h2>
+    </div>
+  );
 }
 
 export default Location;
