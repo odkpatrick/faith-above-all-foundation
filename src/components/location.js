49934c762cc2d4e51c5990d1afb6965612d67889
Patrick Odeke <patrickodekeofficial@gmail.com>
2024-12-06 14:02:24 +0300

    window object ssr issue fix #4

--- src/components/location.js
+++ src/components/location.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+
+// Fix marker icon issue with default icon import
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
+  iconUrl: require('leaflet/dist/images/marker-icon.png').default,
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
+});
 
 const Location = () => {
   const position = [0.351224, 32.594473]; // Replace with your office location coordinates
